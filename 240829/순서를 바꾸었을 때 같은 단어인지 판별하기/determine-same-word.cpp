d116e010d107d3578e093340245bbf5467d990fd
공실 <80194907+gongsiri@users.noreply.github.com>
2024-08-29 17:41:40 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240829/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 240829/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    // sort는 문자열도 가능
+    // 정수형 -> arr+시작주소,arr+끝 주소
+    // 문자형 -> s.begin(), s.end()
     string s1,s2;
     cin>>s1>>s2;
     sort(s1.begin(),s1.end());
