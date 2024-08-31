08d8a344a89ee8ae33b683c0d436d3e01ea28861
공실 <80194907+gongsiri@users.noreply.github.com>
2024-08-31 15:15:00 +0900

    Title: 007, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240831/007/007.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string secret;
+    char place;
+    int time;
+    cin>>secret>>place>>time;
+
+    class Practice{
+        public :
+            string secret;
+            char place;
+            int time;
+
+            Practice(string secret, char place, int time){
+                this->secret = secret;
+                this->place = place;
+                this->time = time;
+            }
+    };
+
+    Practice practice = Practice(secret, place,time);
+    cout<<"secret code : "<<practice.secret<<"\n";
+    cout<<"meeting point : "<<practice.place<<"\n";
+    cout<<"time  : "<<practice.time<<"\n";
+
+    return 0;
+}
