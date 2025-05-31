4828b11099fe96c51bba6298cde2d1d787e29c37
공실 <80194907+gongsiri@users.noreply.github.com>
2025-05-31 22:23:01 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250531/Date to Date/date-to-date.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+int main() {
+    int arr[13] = {0,31,30,31,30,31,30,31,31,30,31,30,31};
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int count = 1;
+    int month = m1;
+    int day = d1;
+
+    while(true){
+        if(month==m2 && day==d2) break;
+        count++;
+        day++;
+        if(day>arr[month]){
+            month++;
+            day=1;
+        }
+    }
+
+    cout<<count;
+
+    // Please write your code here.
+
+    return 0;
+}
