fce85d64cfbf9191ab2dfe566d52531fdc2a4304
공실 <80194907+gongsiri@users.noreply.github.com>
2025-05-31 22:41:28 +0900

    Title: DateTime to DateTime, Time: 7ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 250531/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250531/DateTime to DateTime/datetime-to-datetime.cpp
@@ -6,9 +6,25 @@ int a, b, c;
 
 int main() {
     cin >> a >> b >> c;
-    int sum = c+b*60+a*1440;
-    if(sum<16511) cout<<"-1";
-    else cout<<sum-16511;
 
+    int day = 11, hour = 11, minute = 11;
+    int count = 0;
+
+    while(true){
+        if(day == a && hour == b && minute ==c) break;
+        if(day > a || (day==a && hour>b) || (day==a && hour==b && minute>c)) return -1;
+        count++;
+        minute++;
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+        if(hour == 24){
+            day++;
+            hour = 0;
+        }
+    }
+
+    cout<<count;
     return 0;
 }
